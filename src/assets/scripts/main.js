4d33dbc15c4684d96b04a69f1868c54e5b521d5c
Matt <fortiek@gmail.com>
2022-12-31 00:43:58 -0500

    little more work done for styling, start to work on light mode and accordions next

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,2 +1,10 @@
 // Javascript goes here
 console.log('up to zero!');
+
+const jobs = document.querySelectorAll('.hist-section h2');
+jobs.forEach( job =>{
+    job.addEventListener('click', () => {
+        alert('clicked!');
+        //use this to add accordion functionality
+    });
+});
