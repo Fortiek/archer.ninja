b8b9215e219ca06f0f354d7d6ac541467a5d1aa5
Matt <fortiek@gmail.com>
2023-01-08 20:31:05 -0500

    Get something up on netlify. TODO: implement Spotify API

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,10 +1,31 @@
 // Javascript goes here
 console.log('up to zero!');
 
-const jobs = document.querySelectorAll('.hist-section h2');
-jobs.forEach( job =>{
-    job.addEventListener('click', () => {
-        alert('clicked!');
-        //use this to add accordion functionality
-    });
-});
+//use this to add accordion
+// const jobs = document.querySelectorAll('.hist-section h2');
+// jobs.forEach( job =>{
+//     job.addEventListener('click', () => {
+//         const collapseSection = document.querySelector('.collapse');
+//         collapseSection.classList.toggle('.active');
+
+
+//     });
+// });
+var acc = document.getElementsByClassName("list-title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
---
+++ src/assets/scripts/music.js
@@ -0,0 +1,3 @@
+//spotify connector
+
+const spotify = fetch('https://api.spotify.com/');
