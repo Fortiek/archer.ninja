568a7ed39536025fb94770e7d2fb6553b3399150
Matt Archer <fortiek@gmail.com>
2023-01-31 22:02:12 -0500

    think I fixed the url issue

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -22,10 +22,11 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+    // if (panel.style.display === "block") {
+    //   panel.style.display = "none";
+    // } else {
+    //   panel.style.display = "block";
+    // }
+    panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block";
   });
 }
